413ac97e186c61cf0fd27e76683d6d6cd7079d37
M.S.G <m.sinangokcul@gmail.com>
2026-02-20 16:18:26 +0300

    Initial Commit

---
+++ Zflow/ZflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZflowApp.swift
+//  Zflow
+//
+//  Created by M.Sinan GÖKÇÜL on 20.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ZflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
